0ea06b7d34c9b0567c4d2963c820e8dbffb5a605
Shyamalenduojha17 <shyamalenduojha100@gmail.com>
2023-01-18 22:06:11 +0530

    CRUD-Application

---
+++ mysql/setup.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+  id INT ,
+  name VARCHAR(255) NOT NULL,
+  age INT NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  position VARCHAR(255) NOT NULL,
+  salary INT NOT NULL,
+  PRIMARY KEY (id)
+  );
